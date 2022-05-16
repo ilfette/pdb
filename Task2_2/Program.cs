b6a9bbe6378c416f7b905083f3f6ac9fcf9623b8
ilfette <aefremova-2001@mail.ru>
2022-05-16 14:39:09 +0300

    Task 2_2 code

---
+++ Task2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int delay;
+            Console.WriteLine("Задержка:");
+            Countdown store = new Countdown();
+
+            store.subscribe(new CustomerA("Настя"));
+            store.subscribe(new CustomerB("Иван"));
+            store.subscribe(new CustomerB("Марья"));
+            store.subscribe(new CustomerA("Роман"));
+
+            while (!int.TryParse(Console.ReadLine(), out delay)) { }
+
+            store.notify(delay, "Новый товар!");
+            store.notify(delay, "Скидки");
+            store.notify(delay, "Конец акции");
+            Console.ReadKey();
+
+        }
+    }
+}
