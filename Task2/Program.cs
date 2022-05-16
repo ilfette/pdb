e5446cbe189dc07930c76302ba2052f8896ef089
ilfette <aefremova-2001@mail.ru>
2022-05-16 14:37:19 +0300

    Task 2 code

---
+++ Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using Sorting;
+using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+
+    class Program
+    {
+        static readonly string CHOOSE_SORT_TEXT = "Сортировка: \n" +
+            "1) По сумме \n" +
+            "2) По максимальному элементу \n" +
+            "3) По минимальному значению";
+
+        static readonly string SORT_TYPE_TEXT = "Порядок: \n" +
+            "1) По возрастанию \n" +
+            "2) По убыванию ";
+
+        public static int[][] arr = {
+                            new int[4]{2,9,4,125},
+                            new int[4]{4,3,16,25},
+                            new int[4]{8,1,64,5}
+                           };
+
+        static void Main()
+        {
+            print(arr);
+            while (true)
+            {
+                sort();
+                print(arr);
+            }
+        }
+
+        private static void sort()
+        {
+            var dict = new Dictionary<int, AbstractSortingStrategy>{
+                { 1, new SumSortingStrategy()},
+                { 2, new MaxSortingStrategy()},
+                { 3, new MinSortingStrategy()}
+            };
+
+            Console.WriteLine(CHOOSE_SORT_TEXT);
+            int sortType = int.Parse(Console.ReadLine());
+            SortingDelegate myDelegate = dict[sortType].Sort;
+            Context context = new Context(myDelegate);
+
+            Console.WriteLine(SORT_TYPE_TEXT);
+            int reverse = int.Parse(Console.ReadLine());
+
+            context.Sort(ref arr, reverse != 1);
+        }
+
+        public static void print(int[][] array)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Task2/SortingStrategy.cs
@@ -0,0 +1,106 @@
+﻿using System;
+namespace Sorting
+{
+    public delegate void SortingDelegate(ref int[][] array, bool reverse);
+    public class Context
+    {
+        private SortingDelegate sortingDelegate;
+        public Context(SortingDelegate sortDelegate)
+        {
+            this.sortingDelegate = sortDelegate;
+        }
+
+        public void Sort(ref int[][] array, bool reverse)
+        {
+            sortingDelegate(ref array, reverse);
+        }
+    }
+    public abstract class AbstractSortingStrategy
+    {
+        public void Sort(ref int[][] array, bool reverse)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = i; j < array.GetLength(0); j++)
+                {
+                    if ((compare(array[i], array[j]) == 0) ^ reverse)
+                    {
+                        int[] temp = new int[array[j].Length];
+                        copyArray(array[j], temp);
+                        copyArray(array[i], array[j]);
+                        array[i] = temp;
+                    }
+                }
+            }
+        }
+
+        private void copyArray(int[] source, int[] target)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                target[i] = source[i];
+            }
+        }
+
+        public abstract int compare(int[] array1, int[] array2);
+
+    }
+    public class SumSortingStrategy : AbstractSortingStrategy
+    {
+        public override int compare(int[] first, int[] second)
+        {
+            int s1 = 0;
+            foreach (int i in first)
+                s1 += i;
+            int s2 = 0;
+            foreach (int i in second)
+                s2 += i;
+
+            return s1 <= s2 ? 1 : 0;
+        }
+
+    }
+    public class MaxSortingStrategy : AbstractSortingStrategy
+    {
+        public override int compare(int[] first, int[] second)
+        {
+            int maxS1 = int.MinValue;
+            foreach (int i in first)
+                if (maxS1 < i)
+                {
+                    maxS1 = i;
+                }
+
+            int maxS2 = int.MinValue;
+            foreach (int i in second)
+                if (maxS2 < i)
+                {
+                    maxS2 = i;
+                }
+
+            return maxS1 <= maxS2 ? 1 : 0;
+        }
+    }
+
+    public class MinSortingStrategy : AbstractSortingStrategy
+    {
+        public override int compare(int[] first, int[] second)
+        {
+            int minS1 = int.MaxValue;
+            foreach (int i in first)
+                if (minS1 > i)
+                {
+                    minS1 = i;
+                }
+
+            int minS2 = int.MaxValue;
+            foreach (int i in second)
+                if (minS2 > i)
+                {
+                    minS2 = i;
+                }
+            return minS1 <= minS2 ? 1 : 0;
+        }
+
+    }
+}
